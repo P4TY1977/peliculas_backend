e1d9541a2d6d446316f7298ff2e155b13abd141e
Patricia Medina <f.patricia.medina@gmail.com>
2022-03-18 17:00:31 -0600

    respaldo

---
+++ src/resolvers/peliculas.js
@@ -0,0 +1,15 @@
+const leerPeliculas=()=>Promise.resolve(
+    [
+        {id:1, nombre:'Bambi'},
+        {id:2, nombre:'Pesadilla en la calle del infierno'}
+    ]
+)
+
+const listar = async (context)=>
+{
+  const peliculas = await leerPeliculas()
+  if(!peliculas)
+    return {success:false}
+  return {success: true, peliculas}
+}
+module.exports = {listar}
